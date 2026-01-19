47550ad3bfc201a138d4c594cbcb4470cac736e9
Ragini-Microsoft <v-raginich@microsoft.com>
2026-01-19 13:59:44 +0530

    revert  branch specification from azd init command in install.sh

--- infra/vscode_web/install.sh
+++ infra/vscode_web/install.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt --user -q
 
-azd init -t microsoft/agentic-applications-for-unified-data-foundation-solution-accelerator -b rc-ailocationissue
+azd init -t microsoft/agentic-applications-for-unified-data-foundation-solution-accelerator
